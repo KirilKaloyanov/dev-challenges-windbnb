47d2808ff25fccbe09a38511c470025ed0a4e243
Kiril <kaloyanov.kiril@gmail.com>
2023-02-13 18:54:58 +0200

    Fixing filter vixibility

--- src/filters/guests/guestsValues.js
+++ src/filters/guests/guestsValues.js
@@ -1,9 +1,14 @@
-import styles from './guestsValues.module.css';
+import styles from "./guestsValues.module.css";
 
-export default function GuestsValues({activeFilter}) {
-    return (
-        <div 
-            className={activeFilter==='guests' ? styles.visible : styles.hidden}
-        >hi guest</div>
-    );
+export default function GuestsValues({ expanded, activeFilter }) {
+  return (
+    <div
+      className={
+        `${expanded ? styles.displayed : styles.notDisplayed }
+         ${activeFilter === "guests" ? styles.visible : styles.hidden}`
+      }
+    >
+      hi guest
+    </div>
+  );
 }

fc556743d2d3ee1ce07e0c2bbd603838100a75a3
Kiril <kaloyanov.kiril@gmail.com>
2023-02-13 14:52:17 +0200

    Created guests and location components

--- src/filters/filter.js
+++ src/filters/filter.js
@@ -1,24 +1,31 @@
 import { useState } from "react";
+import Location from "./location/location.js";
+import Guests from "./guests/guests.js";
 import styles from "./filter.module.css";
 
 export default function Filter() {
   const [expanded, setExpanded] = useState(false);
-
-  function expand() {
-    if (expanded) setExpanded(false);
-    else setExpanded(true);
-  }
+  const [activeFilter, setActiveFilter] = useState('');
 
   return (
     <>
       <div
         className={`${styles.container} ${expanded && styles.expandedFilter}`}
-        onClick={expand}
+        onClick={() => setExpanded(true)}
       >
-        <span>Location</span>
-        <span>Guests</span>
-        <button>S</button>
+        <div className={styles.grid}>
+            <Location 
+                expanded={expanded} 
+                onSelect={() => setActiveFilter('location')}
+            />
+            <Guests expanded={expanded} />
+            <button>S</button>
+        </div>
       </div>
+      <div
+        className={`${styles.background} ${expanded && styles.expandedBackground}`}
+        onClick={() => setExpanded(false)}
+      ></div>
     </>
   );
 }

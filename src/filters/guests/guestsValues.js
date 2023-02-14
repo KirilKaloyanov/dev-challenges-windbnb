d5af63877fe509995613f0406b2924d74999cf90
Kiril <kaloyanov.kiril@gmail.com>
2023-02-14 06:52:10 +0200

    Refactor filter visibility

--- src/filters/guests/guestsValues.js
+++ src/filters/guests/guestsValues.js
@@ -3,13 +3,14 @@ import styles from "./guestsValues.module.css";
 export default function GuestsValues({ expanded, activeFilter }) {
   return (
     <div
-      className={`${expanded ? styles.visible : styles.hidden}
-         ${
-           expanded && activeFilter === "guests"
-             ? styles.visible
-             : styles.hidden
-         }
-         ${expanded ? '' : styles.fucked}`}
+      className={`
+            ${
+              expanded && activeFilter === "guests"
+                ? styles.visible
+                : styles.hidden
+            }
+            ${expanded ? "" : styles.notDisplayed}
+         `}
     >
       hi guest
     </div>
--- src/filters/location/locationValues.js
+++ src/filters/location/locationValues.js
@@ -3,13 +3,14 @@ import styles from "./locationValues.module.css";
 export default function LocationValues({ expanded, activeFilter }) {
   return (
     <div
-      className={`${expanded ? styles.visible : styles.hidden}
-         ${
-           expanded && activeFilter === "location"
-             ? styles.visible
-             : styles.hidden
-         }
-         ${expanded ? '' : styles.fucked}`}
+      className={`
+            ${
+              expanded && activeFilter === "location"
+                ? styles.visible
+                : styles.hidden
+            }
+            ${expanded ? "" : styles.notDisplayed}
+         `}
     >
       hi location
     </div>

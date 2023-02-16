ceb31da346203d816c0eb151b51d9c7cd6681ecc
Kiril <kaloyanov.kiril@gmail.com>
2023-02-16 07:19:01 +0200

    Fixed positioning issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Filter from "./filters/filter.js";
-import Card from './Card.js';
+import Card from "./Card.js";
 import logo from "./img/logo.png";
 import "./App.css";
 
@@ -14,19 +14,22 @@ function App() {
     fetch("stays.json")
       .then((data) => data.json())
       .then((response) => {
+        setLocationsList(filterLocations(response));
+        // const accommodations = assignIds(response);
+        // const accommodationsByCapacity = filterByCapacity(accommodations);
+        // const accommodationsByLocation = filterByLocation(accommodationsByCapacity);
         setData(assignIds(response));
-        setLocationsList(filterLocations(response))
       });
   }
 
   function assignIds(arr) {
-    return arr.map((item, index) => ({ id: index, ...item}))
+    return arr.map((item, index) => ({ id: index, ...item }));
   }
 
   function filterLocations(arr) {
     return arr
-    .map((item, i) => ({ id: i, city: item.city, country: item.country }))
-    .filter((b, i, arr) => i === arr.findIndex((c) => c.city === b.city))
+      .map((item, i) => ({ id: i, city: item.city, country: item.country }))
+      .filter((b, i, arr) => i === arr.findIndex((c) => c.city === b.city));
   }
 
   useEffect(() => {
@@ -35,30 +38,49 @@ function App() {
 
   function filterByLocation(item) {
     if (!location) return true;
-    else return item.city === location.city
+    else return item.city === location.city;
   }
 
   function filterByCapacity(item) {
-    return item.maxGuests - guests >=0;
+    return item.maxGuests - guests >= 0;
   }
 
   return (
     <>
-      <Filter
-        guests={guests}
-        location={location}
-        locationsList={locationsList}
-        onGuestsChange={setGuests}
-        onLocationChange={setLocation}
-      />
-      <img className='logo' src={logo} alt="Windbnb logo" />
+      <div className="header">
+        <img className="logo" src={logo} alt="Windbnb logo" />
+        <Filter
+          guests={guests}
+          location={location}
+          locationsList={locationsList}
+          onGuestsChange={setGuests}
+          onLocationChange={setLocation}
+        />
+      </div>
+      <div>
       <h2 className="heading">Stays in Finland</h2>
-      <div className='grid'>
+      <span>{}</span>
+      </div>
+      <div className="grid">
         {data
           .filter(filterByLocation)
           .filter(filterByCapacity)
-          .map((a) => <Card key={a.id} item={a} />)}
+          .map((a) => (
+            <Card key={a.id} item={a} />
+          ))}
       </div>
+
+      <footer>
+        <div>
+          <a
+            href="https://devchallenges.io/portfolio/KirilKaloyanov"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            created by <strong>KirilKaloyanov</strong> - devChallenges.io
+          </a>
+        </div>
+      </footer>
     </>
   );
 }

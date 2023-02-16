35191de181621c8d7c2fc628071c94225414a839
Kiril <kaloyanov.kiril@gmail.com>
2023-02-16 06:19:19 +0200

    Styled the card component

--- src/Card.js
+++ src/Card.js
@@ -6,13 +6,20 @@ export default function Card({ item }) {
       <div className={css.imageContainer}>
         <img src={item.photo} className={css.image} />
       </div>
-      <div>
-        {item.superHost && <span className={css.superHost}>super host</span>}
-        <span></span>
-        <span className={`material-symbols-rounded ${css.red}`}>star</span>
-        <span></span>
+      <div className={css.description}>
+        <span>
+          {item.superHost && <span className={css.superHost}>super host</span>}
+          <span className={css.greyText}>
+            {item.type} &nbsp; &nbsp;
+            {item.beds && <span>{item.beds} beds</span>}
+          </span>
+        </span>
+        <span className={css.rating}>
+          <span className={`material-symbols-rounded ${css.red}`}>star</span>
+          <span>{item.rating}</span>
+        </span>
       </div>
-      <div>{item.city}</div>
+      <div className={css.title}>{item.title}</div>
     </div>
   );
 }
